08f6fbcf4d00f9c5c31fe74887565f16a89c0c5c
PMNServer <31003032+Smallzoamz@users.noreply.github.com>
2026-01-08 21:39:02 +0700

    Fix Vercel DB

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const helmet = require('helmet');
